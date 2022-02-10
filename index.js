ca31f708a6109f5c08c5cb61e364552bf7dd2020
janithBimsaraUlukkulama <jbimsaraulukkulama@outlook.com>
2022-02-10 21:20:38 +0530

    Creating new window

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const electron = require('electron');
 const {app, BrowserWindow, Menu} = electron;
 
-let mainWindow;
+let mainWindow, addWindow;
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({});
@@ -15,18 +15,30 @@ const menuTemplate = [
     {
         label: 'File',
         submenu: [
-            {label: 'New Todo'},
+            {
+                label: 'New Todo',
+                click() {
+                    createAddWindow()
+                }
+            },
             {
                 label: 'Quit',
                 accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
-            click() {
-                app.quit();
+                click() {
+                    app.quit();
+                }
             }
+        ]
     }
-]
+];
+
+const createAddWindow = () => {
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add New TODO'
+    })
 }
-]
-;
 
 if (process.platform === 'darwin') {
     menuTemplate.unshift({});
